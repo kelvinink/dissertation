0665600bb1a0679494f0aa75cb4249cf87c2c772
kelvin <ziyoustep@gmail.com>
2020-02-26 21:19:56 +0800

    Replacing os.popen(curl) with requests lib
    os.open create new subprocess for each call, this cost too many system resources

--- test/ml/test_vader_sentiment.py
+++ test/ml/test_vader_sentiment.py
@@ -1,6 +1,13 @@
-import os
+import requests
+from multiprocessing import Process
 
-while True:
-    res = os.popen("curl http://localhost:80/api/ml/sentiment?sent=your_sentence 2>/dev/null")
-    print(res.read())
-    res.close()
+def test_sentiment(x):
+    seq = 0
+    while True:
+        response = requests.get("http://106.13.90.40:8080/api/ml/sentiment?text=your_sentence")
+        seq += 1
+        print(response.text + "  " + str(seq))
+
+if __name__ == '__main__':
+    for x in range(8):
+        Process(target=test_sentiment, args=(x,)).start()
