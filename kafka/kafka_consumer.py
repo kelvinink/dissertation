ce8ee404971086ce6161170deb227bf6b1002f72
kelvin <ziyoustep@gmail.com>
2020-02-27 14:57:58 +0800

    Update kafka producer/consumer demo

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -1,3 +1,4 @@
+import time
 from kafka import KafkaConsumer
 
 # To consume latest messages and auto-commit offsets
@@ -8,7 +9,17 @@ consumer = KafkaConsumer('kelvin_topic',
 print("List of Topics: ")
 print(consumer.topics())
 
+n = 0
+m = 0
+start = time.time()
+
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print(message)
+    
+    #print(message)
+    n += 1
+    if n == 10000:
+        m += n
+        n = 0
+        print("Msgs: ", m, "  Throughput: ", m / (time.time()-start))
--- kafka/kafka_producer.py
+++ kafka/kafka_producer.py
@@ -1,8 +1,25 @@
+import time
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
+n = 0
+elapsed = 0
 producer = KafkaProducer(bootstrap_servers=['106.13.90.40:19092'])
-future = producer.send('kelvin_topic', b'Hi from kafka')
-producer.flush()
+
+while True:
+    # Async
+    start = time.time()
+    future = producer.send('kelvin_topic', b'Hi from kafka this is a sample message.')
+    end = time.time()
+
+    elapsed += end - start
+    n += 1
+
+    print(future.get())
+    # if n == 10000:
+    #     print("Throughput: ", elapsed / n)
+    #     n = 0
+    #     elapsed = 0
+    
 
 
