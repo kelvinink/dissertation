9b23fe1047a5a5222203345ff4e2ad942957c5e1
kelvinink <kelvinaddr@gmail.com>
2020-03-22 22:27:47 +0800

    Add kafka as data sink

--- data_source/reddit/reddit_streamer.py
+++ data_source/reddit/reddit_streamer.py
@@ -1,23 +1,31 @@
 # For more info please refer to: 
 # https://praw.readthedocs.io/en/latest/tutorials/comments.html
+import datetime
+import json
 
 import praw
-import json
-import os.path
-import datetime
+from kafka import KafkaProducer
 
 from data_source.reddit import reddit_credentials
 
-DELIMITER = ';'
 
-class RedditStreamer():
+bootstrap_servers=['129.204.135.185:19092']
+
+
+class RedditStreamer:
     """
     Streaming and processing live subreddits.
     """
+
     def __init__(self):
         self.count = 0
+        self.producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
-    def stream_comments(self, subreddit, outFile, attrs):
+    def stream2file(self, subreddit, attrs):
+        curr_time = datetime.datetime.now()
+        daymonth = curr_time.strftime("%Y%m%d")
+        outFile = "reddits" + daymonth + ".txt"
+
         reddit = praw.Reddit(client_id=reddit_credentials.CLIENT_ID,
                              client_secret=reddit_credentials.CLIENT_SECRET,
                              user_agent=reddit_credentials.USER_AGENT)
@@ -25,31 +33,37 @@ class RedditStreamer():
         subRed = reddit.subreddit(subreddit)
         with open(outFile, 'a') as f:
             for comment in subRed.stream.comments(skip_existing=False):
-                #print(comment.__dict__)
-                #print(comment.body)
-                try:
-                    print('Reddit ID: ', comment.__dict__['id'], '  count: ', self.count)
-                    self.count += 1
-                    record = []
-                    for attr in attrs:
-                        item = str(comment.__dict__[attr]).replace('\n','').replace(DELIMITER, '')
-                        record.append(item if item is not None else "")
-                    f.write(DELIMITER.join(record) + "\n")
-                except BaseException as e:
-                    print("Error on_data %s" % str(e))
+                self.count += 1
+                print('Reddit ID: ', comment.__dict__['id'], '  count: ', self.count)
+                record = self.parse_comment(comment, attrs)
+                f.write(json.dumps(record) + "\n")
+
+    def stream2kafka(self, subreddit, topic, attrs):
+        reddit = praw.Reddit(client_id=reddit_credentials.CLIENT_ID,
+                             client_secret=reddit_credentials.CLIENT_SECRET,
+                             user_agent=reddit_credentials.USER_AGENT)
+
+        sub_red = reddit.subreddit(subreddit)
+        for comment in sub_red.stream.comments(skip_existing=False):
+            self.count += 1
+            print('Reddit ID: ', comment.__dict__['id'], '  count: ', self.count)
+            record = self.parse_comment(comment, attrs)
+            self.producer.send(topic, json.dumps(record).encode('utf-8'))
+
+    def parse_comment(self, comment, attrs):
+        record = {}
+        for attr in attrs:
+            item = str(comment.__dict__[attr]).replace('\n', '')
+            record[attr] = item if item is not None else ""
+        return record
+
 
 if __name__ == '__main__':
-    attrs = ["id", "created_utc", "link_id", "link_title" , "subreddit_id",
-            "score", "stickied",  "likes", "permalink", "body"]
+    attrs = ["id", "created_utc", "link_id", "link_title", "subreddit_id",
+             "score", "stickied", "likes", "permalink", "body"]
 
-    curr_time = datetime.datetime.now()
-    daymonth = curr_time.strftime("%Y%m%d")
-    
-    outFile = "reddits" + daymonth + ".csv"
-
-    if os.path.isfile(outFile) == False:
-        with open(outFile, "w") as f:
-            f.write(DELIMITER.join(attrs) + "\n")
+    topic = "rcas_raw"
 
     redditStreamer = RedditStreamer()
-    redditStreamer.stream_comments('CryptoCurrency', outFile, attrs)
+    redditStreamer.stream2kafka('CryptoCurrency', topic, attrs)
+    #redditStreamer.stream2file('CryptoCurrency', attrs)
