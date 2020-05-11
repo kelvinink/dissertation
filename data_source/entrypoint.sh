6840e6500eaacb4046facd6185049594d9a667f4
kelvinink <kelvinaddr@gmail.com>
2020-05-11 15:54:55 +0800

    Conainerize data source

---
+++ data_source/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Export python path
+export PYTHONPATH=${PYTHONPATH}:../
+
+# Start the twitter streamer
+python "./twitter/twitter_streamer.py"
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start twitter_streamer: $status"
+  exit $status
+fi
+
+# Start the reddit streamer
+#python "./reddit/reddit_streamer.py"
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to start reddit_streamer: $status"
+#  exit $status
+#fi
